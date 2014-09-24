5aec90785b0bfa5b79638597ef888cc8475a1ce9
jin <jin@crypt.sg>
2014-09-24 13:03:43 +0800

    basic skeleton

--- src/app/Main.java
+++ src/app/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
         primaryStage.setTitle("wat do?");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 550, 700));
         primaryStage.show();
     }
 
