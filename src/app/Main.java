f4605607ad7c7414e5b9a3d64bbcb7ac856c185d
jin <jin@crypt.sg>
2014-09-24 17:58:46 +0800

    Subclass ListCell to create TaskListCellController, in order to have a gridpane as individual cell layout

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,7 @@ package app;
 
 import app.controllers.InputFieldController;
 import app.controllers.SidebarController;
+import app.controllers.TaskListViewController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -19,6 +20,7 @@ public class Main extends Application {
     private BorderPane rootLayout;
 
     private TextField inputField;
+    private ListView taskListView;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -35,9 +37,11 @@ public class Main extends Application {
         try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(getClass().getResource("views/TaskListView.fxml"));
-            ListView taskListView = loader.load();
+            taskListView = loader.load();
 
             rootLayout.setCenter(taskListView);
+            TaskListViewController controller = loader.getController();
+            controller.setMainApp(this);
         } catch (IOException e) {
             e.printStackTrace();
         }
