4dee0cb19a1916baa07163dddf318b1c28f5e92a
Joshua Lee <me@jolly.sg>
2014-09-25 23:37:14 +0800

    removed unused imports from todoitemlist

--- src/app/model/TodoItemList.java
+++ src/app/model/TodoItemList.java
@@ -14,8 +14,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.ListIterator;
