7efc683bcdcc1b51beb4fb39091ec2008370a2d9
jin <jin@crypt.sg>
2014-09-28 19:22:16 +0800

    Scroll to the bottom of the list when a new task is added

--- src/app/controllers/TaskListViewController.java
+++ src/app/controllers/TaskListViewController.java
@@ -2,6 +2,11 @@ package app.controllers;
 
 import app.Main;
 import app.model.TodoItem;
+import com.sun.tools.javac.comp.Todo;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,6 +24,8 @@ public class TaskListViewController {
     private Label placeholder;
 
     private Main main;
+
+    private ObservableList<TodoItem> taskData = FXCollections.observableArrayList();
 
     @FXML
     public void initialize() {
@@ -38,7 +45,22 @@ public class TaskListViewController {
         taskListView.setPlaceholder(placeholder);
     }
 
-    public void updateView(ObservableList<TodoItem> taskData) { taskListView.setItems(taskData); }
+    public void updateView(ObservableList<TodoItem> taskData) {
+        if (newTaskAdded(taskData, this.taskData)) {
+            scrollToLast();
+        }
+        this.taskData = taskData;
+        taskListView.setItems(taskData);
+
+    }
+
+    private boolean newTaskAdded(ObservableList<TodoItem> _new, ObservableList<TodoItem> _old) {
+       return _new.size() == _old.size() + 1;
+    }
+
+    private void scrollToLast() {
+        taskListView.scrollTo(taskData.size());
+    }
 
     /**
      * Is called by the main application to give a reference back to itself.
