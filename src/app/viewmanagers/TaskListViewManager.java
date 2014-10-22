11e5ece2b2791f8db2116c04e394faff34105b8e
jin <jin@crypt.sg>
2014-10-22 22:51:51 +0800

    The task list now scrolls to, selects and focus the last modified task

--- src/app/viewmanagers/TaskListViewManager.java
+++ src/app/viewmanagers/TaskListViewManager.java
@@ -3,6 +3,7 @@ package app.viewmanagers;
 import app.helpers.LoggingService;
 import app.helpers.UserGuide;
 import app.model.TodoItem;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -17,9 +18,6 @@ public class TaskListViewManager {
 
     @FXML
     public ListView<TodoItem> taskListView;
-
-    @FXML
-    private Label placeholder;
 
     private RootViewManager rootViewManager;
 
@@ -60,25 +58,19 @@ public class TaskListViewManager {
         assert(taskData.size() >= 0);
         assert(taskData.size() <= Integer.MAX_VALUE);
 
-        if (newTaskAdded(taskData, this.taskData)) {
-            scrollToLast();
-        }
         this.taskData = taskData;
         taskListView.setItems(taskData);
+
+        scrollToLastModifiedTask();
         LoggingService.getLogger().log(Level.INFO, "Refreshed task list.");
     }
 
-    private boolean newTaskAdded(ObservableList<TodoItem> _new, ObservableList<TodoItem> _old) {
-        return _new.size() > _old.size();
+    private void scrollToLastModifiedTask() {
+        int index = rootViewManager.getMainApp().getTaskController().getLastModifiedIndex();
+        taskListView.scrollTo(index);
+        taskListView.getSelectionModel().select(index);
+        taskListView.getFocusModel().focus(index);
     }
-
-    private void scrollToLast() {
-        taskListView.scrollTo(taskData.size());
-    }
-
-    private void scrollToAddedTask() {
-    }
-
 
     public void setRootViewManager(RootViewManager rootViewManager) {
         this.rootViewManager = rootViewManager;
