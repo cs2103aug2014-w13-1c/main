5ef1c61c6024a24ec142f22daec78b4b6e8d2dc5
jin <jin@crypt.sg>
2014-10-24 00:08:05 +0800

    Wrote unit tests for LoggingService

---
+++ src/app/tests/LoggingServiceTest.java
@@ -0,0 +1,55 @@
+import app.helpers.LoggingService;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by jin on 23/10/14.
+ */
+public class LoggingServiceTest {
+
+    /**
+     * Test if the logger object returned by LoggingService
+     * is always the same object, therefore writing to the same
+     * log file.
+     */
+    @Test
+    public void testSingleton() {
+        Logger logger = LoggingService.getLogger();
+        Logger logger2 = LoggingService.getLogger();
+        assertThat(logger, is(logger2));
+    }
+
+    /**
+     * Test that the file is created by LoggingService
+     * and it is readable.
+     */
+    @Test
+    public void testLogfileCreation() {
+        Logger logger = LoggingService.getLogger();
+        logger.log(Level.INFO, "Test log");
+
+        File log = new File("./logs/watdo.log"); // This does not create a file
+        assert(log.isFile() && log.canRead());
+        log.delete();
+    }
+
+    /**
+     * Test that the logger successfully logs and entry into the file.
+     */
+    @Test
+    public void testLogsWritten() {
+        File log = new File("./logs/watdo.log");
+        log.delete();
+        assertThat(log.exists(), is(false));
+
+        LoggingService.getLogger().log(Level.INFO, "foo");
+        assertThat(log.length() == 0, is(false));
+    }
+
+
+}
