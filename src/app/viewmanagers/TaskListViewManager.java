86d0cfc958b226f17866c9a5e3f5c43a0eb81a01
Joshua Lee <me@jolly.sg>
2014-11-03 21:10:19 +0800

    added getter for taskData in TaskListViewManager

--- src/app/viewmanagers/TaskListViewManager.java
+++ src/app/viewmanagers/TaskListViewManager.java
@@ -120,4 +120,8 @@ public class TaskListViewManager {
         this.rootViewManager = rootViewManager;
     }
 
+    public ObservableList<TodoItem> getTaskData() {
+        return taskData;
+    }
+
 }
