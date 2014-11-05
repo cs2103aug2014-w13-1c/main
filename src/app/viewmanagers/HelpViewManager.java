72c9f49ab855336ae2a6b254b3dfc5002a4d5180
Joshua Lee <me@jolly.sg>
2014-11-05 20:53:44 +0800

    updated help view, all caps-ed titlebar title

--- src/app/viewmanagers/HelpViewManager.java
+++ src/app/viewmanagers/HelpViewManager.java
@@ -2,6 +2,7 @@ package app.viewmanagers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 /**
  * Created by jolly on 10/10/14.
@@ -14,7 +15,27 @@ public class HelpViewManager {
     private RootViewManager rootViewManager;
 
     @FXML
+    private Label helpCommands;
+
+    @FXML
     private void initialize() {
+        helpCommands.setText("Here are the other commands you can use:\n" +
+                             "\n" +
+                             "add <task name> [start | end | priority]\n" +
+                             "delete <task index>\n" +
+                             "done <task index>\n" +
+                             "undone <task index>\n" +
+                             "update <task index> [task name | start | end | priority]\n" +
+                             "search <task name>\n" +
+                             "search [start | end]\n" +
+                             "sort [start / end / priority]\n" +
+                             "display [done / all / overdue]\n" +
+                             "undo\n" +
+                             "redo\n" +
+                             "help\n" +
+                             "settings\n" +
+                             "clear\n" +
+                             "exit");
         okButton.setOnAction((event) -> rootViewManager.closeHelp());
     }
 
