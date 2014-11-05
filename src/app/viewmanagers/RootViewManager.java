38dd2996a13ec6be970a4e91065b5d4a071dee23
Joshua Lee <me@jolly.sg>
2014-11-05 12:35:23 +0800

    fixed bring focus onto inputfield on app launch

--- src/app/viewmanagers/RootViewManager.java
+++ src/app/viewmanagers/RootViewManager.java
@@ -199,6 +199,8 @@ public class RootViewManager {
             inputFieldViewManager.setFromButton(false);
             inputField.positionCaret(text.length());
             inputField.requestFocus();
+        } else {
+            inputField.requestFocus();
         }
     }
 
