0d818ac55133e172447f02b15e29452cc545033a
jin <jin@crypt.sg>
2014-11-06 21:14:24 +0800

    Simplify return value of CommandController's convertList

--- src/app/controllers/CommandController.java
+++ src/app/controllers/CommandController.java
@@ -227,8 +227,7 @@ public class CommandController {
     }
 
     public ObservableList<TodoItem> convertList(ArrayList<TodoItem> todoList) {
-        ObservableList<TodoItem> taskData = FXCollections.observableArrayList(todoList);
-        return taskData;
+        return FXCollections.observableArrayList(todoList);
     }
 
     public void updateView() {
