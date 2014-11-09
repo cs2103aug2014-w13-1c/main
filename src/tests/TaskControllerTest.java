dad12ae8ccd6edbc12e3a962bbe0f9d38e37704a
Joshua Lee <me@jolly.sg>
2014-11-10 00:02:56 +0800

    finished writing tests for filtering methods of taskcontroller

--- src/tests/TaskControllerTest.java
+++ src/tests/TaskControllerTest.java
@@ -9,6 +9,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Date;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -21,6 +23,7 @@ public class TaskControllerTest extends Main {
 
     /**
      * Setup needed to run tests, includes creating a new CommandController and TaskController.
+     * Changes to test directory.
      */
     @Before
     public void setUp() {
@@ -35,10 +38,12 @@ public class TaskControllerTest extends Main {
 
     /**
      * Teardown for tests, clears the task list and sets commandController and taskController to null.
+     * Changes back to default directory.
      */
     @After
     public void tearDown() {
         commandController.parseCommand("clear");
+        commandController.parseCommand("saveto ./");
         commandController = null;
         taskController = null;
     }
@@ -76,7 +81,7 @@ public class TaskControllerTest extends Main {
     }
 
     /**
-     * testing the ability to search within all task names
+     * Testing the ability to search within all task names.
      *
      * @throws Exception any exception
      */
@@ -85,6 +90,7 @@ public class TaskControllerTest extends Main {
         // testing with an empty task list
         assertEquals(taskController.instantSearch("task").size(), 0);
 
+        // testing search for tasks
         commandController.parseCommand("add task 111");
         commandController.parseCommand("add task 222");
         commandController.parseCommand("add task 333");
@@ -94,7 +100,7 @@ public class TaskControllerTest extends Main {
     }
 
     /**
-     * testing the ability to filter all tasks
+     * Testing the ability to filter all tasks.
      *
      * @throws Exception any exception
      */
@@ -103,21 +109,24 @@ public class TaskControllerTest extends Main {
         // testing with an empty task list
         assertEquals(taskController.getAllTasks().size(), 0);
 
+        // testing that added tasks are in all tasks
         commandController.parseCommand("add task 111");
         commandController.parseCommand("add task 222");
         commandController.parseCommand("add task 333");
         assertEquals(taskController.getAllTasks().size(), 3);
 
+        // testing that deleting tasks results in less tasks
         commandController.parseCommand("delete 2");
         assertEquals(taskController.getAllTasks().size(), 2);
 
+        // testing for more adding of tasks
         commandController.parseCommand("add task 444");
         commandController.parseCommand("add task 555");
         assertEquals(taskController.getAllTasks().size(), 4);
     }
 
     /**
-     * testing the ability to filter done tasks
+     * Testing the ability to filter done tasks.
      *
      * @throws Exception any exception
      */
@@ -131,14 +140,14 @@ public class TaskControllerTest extends Main {
         commandController.parseCommand("add task 333");
         assertEquals(taskController.getDoneTasks().size(), 0);
 
-        // testing for boundary case of 2 done tasks
+        // testing for 2 done tasks
         commandController.parseCommand("done 1");
         commandController.parseCommand("done 1");
         assertEquals(taskController.getDoneTasks().size(), 2);
     }
 
     /**
-     * testing the ability to filter undone tasks
+     * Testing the ability to filter undone tasks.
      *
      * @throws Exception any exception
      */
@@ -152,7 +161,7 @@ public class TaskControllerTest extends Main {
         commandController.parseCommand("add task 333");
         assertEquals(taskController.getUndoneTasks().size(), 3);
 
-        // testing for boundary case of 2 undone tasks
+        // testing for 2 undone tasks
         commandController.parseCommand("done 3");
         commandController.parseCommand("done 1");
         commandController.parseCommand("display done");
@@ -161,7 +170,7 @@ public class TaskControllerTest extends Main {
     }
 
     /**
-     * testing the ability to filter overdue tasks
+     * Testing the ability to filter overdue tasks.
      *
      * @throws Exception any exception
      */
@@ -170,25 +179,65 @@ public class TaskControllerTest extends Main {
         // first we test that in an empty task list, the number of overdue tasks is 0
         assertEquals(taskController.getOverdueTasks().size(), 0);
 
-        // testing for boundary case of 2 overdue tasks
+        // testing for 2 overdue tasks
         commandController.parseCommand("add task 111 end yesterday");
         commandController.parseCommand("add task 222 end tomorrow");
         commandController.parseCommand("add task 333 end yesterday");
         assertEquals(taskController.getOverdueTasks().size(), 2);
     }
 
-//    @Test
-//    public void testGetTasksStartingOn() throws Exception {
-//
-//    }
+    /**
+     * Testing the ability to filter tasks starting on a certain day.
+     *
+     * @throws Exception any exception
+     */
+    @Test
+    public void testGetTasksStartingOn() throws Exception {
+        // first we test that in an empty task list, the number of tasks falling on today is 0
+        assertEquals(taskController.getTasksStartingOn(new Date()).size(), 0);
 
-//    @Test
-//    public void testGetTasksEndingOn() throws Exception {
-//
-//    }
+        // testing that it will only find tasks that end on the day itself
+        commandController.parseCommand("add task 111 start today");
+        commandController.parseCommand("add task 222 start tomorrow");
+        commandController.parseCommand("add task 333 start yesterday");
+        assertEquals(taskController.getTasksStartingOn(new Date()).size(), 1);
+    }
 
-//    @Test
-//    public void testGetTasksWithinDateRange() throws Exception {
-//
-//    }
+    /**
+     * Testing the ability to filter tasks ending on a certain day.
+     *
+     * @throws Exception any exception
+     */
+    @Test
+    public void testGetTasksEndingOn() throws Exception {
+        // first we test that in an empty task list, the number of tasks falling on today is 0
+        assertEquals(taskController.getTasksEndingOn(new Date()).size(), 0);
+
+        // testing that it will only find tasks that end on the day itself
+        commandController.parseCommand("add task 111 end today");
+        commandController.parseCommand("add task 222 end tomorrow");
+        commandController.parseCommand("add task 333 end yesterday");
+        assertEquals(taskController.getTasksEndingOn(new Date()).size(), 1);
+    }
+
+    /**
+     * Testing the ability to filter tasks that fall within the given date range.
+     *
+     * @throws Exception any exception
+     */
+    @Test
+    public void testGetTasksWithinDateRange() throws Exception {
+        // first we test that in an empty task list, the number of tasks falling on today is 0
+        assertEquals(taskController.getTasksWithinDateRange(new Date(), new Date()).size(), 0);
+
+        // testing that it will only find tasks within the date range
+        commandController.parseCommand("add task 111 start today");
+        commandController.parseCommand("add task 222 start tomorrow");
+        commandController.parseCommand("add task 333 start yesterday");
+        commandController.parseCommand("add task 444 end today");
+        commandController.parseCommand("add task 555 end tomorrow");
+        commandController.parseCommand("add task 666 end yesterday");
+        commandController.parseCommand("add task 777 start last week end next week");
+        assertEquals(taskController.getTasksWithinDateRange(new Date(), new Date()).size(), 3);
+    }
 }
